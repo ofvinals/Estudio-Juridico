7cafcfd0439c66ad45b70a30ceff0c8fd0167676
Oscar <ofvinals@gmail.com>
2024-01-12 17:33:26 -0300

    arreglo imagen ppal de compts

--- src/index.js
+++ src/index.js
@@ -34,9 +34,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: 'http://localhost:5173',
-		origin: 'https://flourishing-tanuki-55bdc2.netlify.app',
-		
+		origin: ['http://localhost:5173', 'https://flourishing-tanuki-55bdc2.netlify.app'], 
 		credentials: true,
 		optionsSuccessStatus: 200,
 	})
